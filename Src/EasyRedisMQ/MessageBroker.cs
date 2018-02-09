a97711c1707941f170c5602408d698baca0a5b68
Alberto Malavassi <alberto.malavassi@fiserv.com>
2018-02-09 15:08:24 -0600

    netstandard 2.0 compatibility

--- Src/EasyRedisMQ/MessageBroker.cs
+++ Src/EasyRedisMQ/MessageBroker.cs
@@ -27,7 +27,12 @@ namespace EasyRedisMQ
             _exchangeSubscriberService = exchangeSubscriberService;
             _notificationService = notificationService;
             _subscriberFactory = subscriberFactory;
+
+#if NET462
             _memoryCache = MemoryCache.Default;
+#else
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+#endif
         }
 
         public async Task PublishAsync<T>(T message) where T : class
@@ -56,6 +61,7 @@ namespace EasyRedisMQ
             await _exchangeSubscriberService.AddSubscriberAsync(subScriber);
         }
 
+#if NET462
         private async Task<List<SubscriberInfo>> GetSubscriberInfosAsync<T>() where T : class
         {
             var typeName = typeof(T).FullName;
@@ -65,5 +71,21 @@ namespace EasyRedisMQ
             _memoryCache.Add(typeName, subscriberInfos, DateTimeOffset.Now.AddSeconds(15));
             return subscriberInfos;
         }
+#else
+        private async Task<List<SubscriberInfo>> GetSubscriberInfosAsync<T>() where T : class
+        {
+            var typeName = typeof(T).FullName;
+
+            var subscriberInfos = new List<SubscriberInfo>();
+
+            // Look for cache key.
+            if (!_memoryCache.TryGetValue(typeName, out subscriberInfos))
+            {
+                subscriberInfos = await _exchangeSubscriberService.GetSubscriberInfosAsync<T>();
+                _memoryCache.Set(typeName, subscriberInfos, DateTimeOffset.Now.AddSeconds(15));
+            }
+            return subscriberInfos;
+        }
+#endif
     }
 }
